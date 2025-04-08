1a42b65b62ea6084c8e9ef3dabed45db8374a73a
Bob O'Keefe <101881869+bob-okeefe@users.noreply.github.com>
2025-04-08 18:38:11 +0000

    Edits for Excercise 3

--- octofit-tracker/backend/octofit_tracker/models.py
+++ octofit-tracker/backend/octofit_tracker/models.py
@@ -1,40 +1,28 @@
 from djongo import models
 
 class User(models.Model):
+    _id = models.ObjectIdField()
+    username = models.CharField(max_length=100)
     email = models.EmailField(unique=True)
-    name = models.CharField(max_length=255)
-    created_at = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return self.email
+    password = models.CharField(max_length=100)
 
 class Team(models.Model):
-    name = models.CharField(max_length=255)
-    members = models.ArrayField(model_container=User)
-
-    def __str__(self):
-        return self.name
+    _id = models.ObjectIdField()
+    name = models.CharField(max_length=100)
+    members = models.ArrayReferenceField(to=User, on_delete=models.CASCADE)
 
 class Activity(models.Model):
+    _id = models.ObjectIdField()
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    activity_type = models.CharField(max_length=255)
-    duration = models.IntegerField()  # in minutes
-    date = models.DateField()
-
-    def __str__(self):
-        return f"{self.user.email} - {self.activity_type}"
+    activity_type = models.CharField(max_length=100)
+    duration = models.DurationField()
 
 class Leaderboard(models.Model):
-    team = models.ForeignKey(Team, on_delete=models.CASCADE)
-    points = models.IntegerField()
-
-    def __str__(self):
-        return f"{self.team.name} - {self.points} points"
+    _id = models.ObjectIdField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    score = models.IntegerField()
 
 class Workout(models.Model):
-    name = models.CharField(max_length=255)
+    _id = models.ObjectIdField()
+    name = models.CharField(max_length=100)
     description = models.TextField()
-    duration = models.IntegerField()  # in minutes
-
-    def __str__(self):
-        return self.name
